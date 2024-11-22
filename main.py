fa6529befe6fa0e1e6b43af350c4f10df77af1d7
HalitcanEmir <halitcanemir06@gmail.com>
2024-11-22 21:35:16 +0300

    first commit for first project

---
+++ main.py
@@ -0,0 +1,25 @@
+mesaj  = "yapay zeka alani"
+
+mesaj2 = " kendimi geliştirmek isetiğim yer"
+
+mesaj = mesaj.upper()
+
+isim = "Yılmaz Can Tok"
+
+yas = "26"
+
+
+
+print("Bir başlangıç\nGünlerce devam edicek kod yazma serime böyle bir başlangıç yapıyorum")
+
+print(mesaj+mesaj2)
+
+mesaj = mesaj.lower()
+
+print(mesaj.startswith("yap"))
+
+print(mesaj2.startswith("kaş"))
+
+print(len(mesaj + mesaj2))
+
+print("ismin {} {} yaşındasın".format(isim, yas))
